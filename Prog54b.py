7f9d89191ffb1880d1276c840deafa684bbc9381
chelseyq <112962892+chelseyqian@users.noreply.github.com>
2023-01-23 14:43:04 +0000

    update

---
+++ Prog54b.py
@@ -0,0 +1,9 @@
+n1=int(input("Enter the first number: "))
+n2=int(input("Enter the second number: "))
+n3=int(input("Enter the third number: "))
+n4=int(input("Enter the fourth number: "))
+
+sum=n1+n2+n3+n4
+ave=sum/4
+
+print("Sum is ",sum,"\nAverage is ",ave)
---
+++ Prog54c.py
@@ -0,0 +1,8 @@
+r = float(input("Enter the radius: "))
+
+area=3.14159 * r * r
+cir=2 * 3.14159 * r
+
+print("radius is ",r)
+print("area is ",area)
+print("circumference is ",cir)
