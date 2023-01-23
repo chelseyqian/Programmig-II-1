085d86ad57c38b8b0cb1ad952fa97a9825827cf3
chelseyq <112962892+chelseyqian@users.noreply.github.com>
2023-01-23 14:44:18 +0000

    update

--- Prog54c.py
+++ Prog54c.py
@@ -3,6 +3,6 @@ r = float(input("Enter the radius: "))
 area=3.14159 * r * r
 cir=2 * 3.14159 * r
 
-print("radius is ",r)
-print("area is ",area)
-print("circumference is ",cir)
+print("radius is ", r)
+print("area is ", area)
+print("circumference is ", cir)
