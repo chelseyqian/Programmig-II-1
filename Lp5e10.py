394b8b2b9a255807e51690483379a886d99849db
chelseyq <112962892+chelseyqian@users.noreply.github.com>
2023-01-31 15:52:52 +0000

    update

--- Lp5e10.py
+++ Lp5e10.py
@@ -1,5 +1,14 @@
 n1 = int(input("Enter the first number: "))
-n2 = int(input("Enter the second number :"))
-gcd = 0
+n2 = int(input("Enter the second number: "))
+gcd = 1
 
-gcd = math.gcd(n1, n2)
+large = n1
+if n1 > n2:
+  large = n2
+
+while not (n1 % large == 0 and n2 % large == 0):
+  large -= 1
+
+gcd = large
+
+print("The GCD is " + str(gcd))
