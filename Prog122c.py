04c5a406781ac8be61430fa45998fb047ee7df80
chelseyq <112962892+chelseyqian@users.noreply.github.com>
2023-02-01 14:24:27 +0000

    update

--- Prog122c.py
+++ Prog122c.py
@@ -1,6 +1,6 @@
-for i in range(2, 11):
-    if i%2==0:
-        column2=i+1
-        column3=i*2
-        column4=i**2
-        print(i, '\t', column2, '\t', column3, '\t', column4)
+# range is range(start, stop, step)
+for i in range(2, 11, 2):
+  column2=i+1
+  column3=i*2
+  column4=i**2
+  print(i, '\t', column2, '\t', column3, '\t', column4)
