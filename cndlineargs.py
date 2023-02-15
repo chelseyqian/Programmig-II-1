af3e2c4a03c2272dd19e2c42ac825f2628af4f7b
chelseyq <112962892+chelseyqian@users.noreply.github.com>
2023-02-15 14:19:16 +0000

    update

---
+++ cndlineargs.py
@@ -0,0 +1,13 @@
+import sys  #provide access to command line args
+
+def main(args):
+  # C-style "Main/Entrypoint" Function
+  if len(args) <= 0:
+    print("Hello!")
+    return 
+  print("Hello,", args[0])
+  for arg in args:
+    print(arg)
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
