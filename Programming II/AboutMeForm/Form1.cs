b6741402599fc4c16dce7171c8c36191c52b5bf9
chelseyqian <chelsey.yihuiqian@gmail.com>
2023-04-12 08:40:56 -0500

    update

---
+++ Programming II/AboutMeForm/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AboutMeForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label4.Text = "Chelsey";
+            label5.Text = "???";
+            label6.Text = "?????";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label4.Text = "";
+            label5.Text = "";
+            label6.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+    }
+}
