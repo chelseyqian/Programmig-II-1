1a1e302f9d3bbefb14efc48cda95065b9adf504e
chelseyqian <chelsey.yihuiqian@gmail.com>
2023-04-25 08:36:19 -0500

    update

---
+++ Programming II/Lang122d/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lang122d
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox1.Items.Add("x\t\ty");
+            for (int i = -12; i <= 16; i++)
+            {
+                double y = Math.Pow(i, 6) - 3 * Math.Pow(i, 5) - 93 * Math.Pow(i, 4) 
+                    + 87 * Math.Pow(i, 3) + 1596 * Math.Pow(i, 2) - 1380 * i - 2800;
+                listBox1.Items.Add(i + "\t\t" + (int)y);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Programming II/Pg347Sum/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pg347Sum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = int.Parse(textBox1.Text);
+            int sum = 0;
+            if (num <= 0)
+            {
+                label2.Text = "The number is invalid";
+            } else {
+                for (int i = 1; i <= num; i++) {
+                    sum += i;
+                }
+                label2.Text = "The sum is " + sum.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
