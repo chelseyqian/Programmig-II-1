ecd81d7df57667d4e7a6b86bc24708b137444232
chelseyqian <chelsey.yihuiqian@gmail.com>
2023-05-12 08:31:52 -0500

    update

--- Programming II/Pg498PayRoll/Form1.cs
+++ Programming II/Pg498PayRoll/Form1.cs
@@ -24,7 +24,33 @@ namespace Pg498PayRoll
 
         private void button1_Click(object sender, EventArgs e)
         {
+            int[] hours = new int[MAX_EMPLOYEES];
+            int lcv = 0;
+            int empHours = 0;
+            decimal empPay = 0.0m;
 
+            for (lcv = 0; lcv < MAX_EMPLOYEES; lcv++)
+            {
+                while (int.TryParse
+                (
+                    Interaction.InputBox("Enter the hours worked by employee #" + 
+                        (lcv + 1).ToString(), "Need hours worked"), 
+                        out empHours) == false)
+                    MessageBox.Show("Please enter an integer for hours worked");
+                hours[lcv] = empHours;
+            }
+
+            listBox1.Items.Clear();
+            for (lcv = 0; lcv < MAX_EMPLOYEES; lcv++)
+            {
+                empPay = hours[lcv] * HOURLY_PAY_RATE;
+                listBox1.Items.Add("Employee " + (lcv + 1).ToString() + " earned " + empPay.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
