1d77d591135508ab7472911460b844e3e99894e1
chelseyqian <chelsey.yihuiqian@gmail.com>
2023-05-15 08:19:08 -0500

    u

---
+++ Programming II/StrInterview18/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrInterview18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string str = Console.ReadLine();
+            Console.Write("Enter a character to remove: ");
+            char c = char.Parse(Console.ReadLine());
+            string result = "";
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != c)
+                {
+                    result += str[i];
+                }
+            }
+
+            Console.WriteLine("The string is " + result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Programming II/StrInterview8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrInterview8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.Write("Enter a string: ");
+            string str = Console.ReadLine();
+            Console.Write("Enter a character in the string: ");
+            char c = char.Parse(Console.ReadLine());
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == c)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("The number of occurence is " + count);
+            Console.ReadLine();
+        }
+    }
+}
